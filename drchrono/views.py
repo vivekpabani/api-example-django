1b3bca82eb973413a1b85f112456457c0030246f
Vivek Pabani <vpabani@hawk.iit.edu>
2017-02-24 17:40:16 -0600

    Fetch patient data in home view

--- drchrono/views.py
+++ drchrono/views.py
@@ -3,8 +3,15 @@ from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 
-
+@login_required
 def home(request):
+    """
+    home view to display patient data whose birthday is nearby.
+    """
+
+    access_token = request.user.social_auth.get(provider='drchrono').extra_data['access_token']
+
+    patients = get_patient_data(access_token)
 
     return render(request, 'home.html')
 
