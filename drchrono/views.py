d1012b052ed0f2b5272b14b7114cf78be43ddf53
Vivek Pabani <vpabani@hawk.iit.edu>
2017-02-24 16:51:55 -0600

    Add login and logout view

--- drchrono/views.py
+++ drchrono/views.py
@@ -1,1 +1,15 @@
-# Create your views here.
+from django.contrib.auth import logout as auth_logout
+from django.shortcuts import redirect, render
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+
+def login_view(request):
+
+    return render(request, 'login.html')
+
+@login_required
+def logout_view(request):
+
+    auth_logout(request)
+
+    return redirect('/')
