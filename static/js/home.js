bc0a340c8268f1f1c7ee5f791648d81beca725e5
Vivek Pabani <vpabani@hawk.iit.edu>
2017-02-24 18:11:29 -0600

    Add form submit listener to send mail when form submitted without refreshing page

--- static/js/home.js
+++ static/js/home.js
@@ -1,5 +1,4 @@
 
-
 function showPatientDetails(id, first_name, last_name, email, contact_num, profile_pic, address, date_of_birth, greeting_message){
 
     var patient_info = document.getElementById('patient_info');
@@ -37,5 +36,36 @@ function showPatientDetails(id, first_name, last_name, email, contact_num, profi
         submit_button.disabled = true;
         text_area.disabled = true;
     }
-
 }
+
+$(document).ready(function() {
+    $('.greet_form').submit(function() { // catch the form's submit event
+        var greet_id = this.greet_id.value;
+        $.ajax({ // create an AJAX call...
+            async: true,
+            data: $(this).serialize(), // get the form data
+            type: $(this).attr('method'), // GET or POST
+            url: 'send_email/', // the url to call
+            success: function(response) { // on success..
+                var manual_form_greet_id = document.getElementById('greet_id')
+                if (manual_form_greet_id  != null && manual_form_greet_id.value == greet_id){
+
+                    var submit_button = document.getElementById('greet_submit');
+                    submit_button.value = "Greeted";
+                    submit_button.disabled = true;
+
+                    var text_area = document.getElementById('greet_message');
+                    text_area.disabled = true;
+                }
+
+                var auto_greet_button = document.getElementById('greet_'+greet_id);
+                auto_greet_button.value = "Greeted";
+                auto_greet_button.disabled = true;
+            },
+            error: function(e, x, r) { // on error..
+            }
+        });
+        return false;
+    });
+
+});
