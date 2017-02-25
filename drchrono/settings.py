713b0d269de382dc80cb649d98a46e33cc8e6cb5
Vivek Pabani <vpabani@hawk.iit.edu>
2017-02-24 21:24:12 -0600

    Add secret key when not debug

--- drchrono/settings.py
+++ drchrono/settings.py
@@ -37,8 +37,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 #SECRET_KEY = "Your secret key from secret config"
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
+if not DEBUG:
+    SECRET_KEY = os.environ['SECRET_KEY']
 
 ALLOWED_HOSTS = ['*']
 
